5afa2757cbf13ce7a18f32b2929ecd7eecc8ac85
Domingo García <domingo.garcia@sage.com>
2022-04-21 20:11:26 +0200

    First load

---
+++ eth-contracts/contracts/Ownable.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+contract Ownable {
+
+    address private _owner;
+        
+    event ownershipTransfered(address newOwner);
+
+    function getOwner() view returns(address) {
+        return _owner;
+    }
+
+    constructor() internal {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Only the owner can do this action");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(_owner != newOwner, "New owner must be different from current owner");
+        require(msg.sender == tx.origin, "A contract cannot make this operation");
+
+        _owner = newOwner;
+        emit ownershipTransfered(_owner);
+    }
+}
